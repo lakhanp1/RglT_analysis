1452a9098b7800ee15f3139062d6894d66bdaf0b
Lakhansing Pardeshi <lakhanp1@gmail.com>
2019-09-13 19:52:20 +0800

    topGO for 6 DiffBind groups

--- scripts/14_diffbind_GO_enrichment.R
+++ scripts/14_diffbind_GO_enrichment.R
@@ -4,27 +4,28 @@ library(cowplot)
 
 rm(list = ls())
 
-source(file = "E:/Chris_UM/GitHub/omics_util/GO_enrichment/topGO_functions.R")
+source(file = "E:/Chris_UM/GitHub/omics_util/04_GO_enrichment/topGO_functions.R")
 
-outDir <- here::here("analysis", "ChIPseq_analysis", "diffbind", "topGO")
+outDir <- here::here("analysis", "02_ChIPseq_analysis", "03_diffbind", "topGO")
 
 if(!dir.exists(outDir)){
   dir.create(path = outDir)
 }
 
-outPrefix <- paste(outDir, "/topGO_goodPeaks.", sep = "")
+
+outPrefix <- paste(outDir, "/topGO", sep = "")
 
 ##################################################################################
 diffbindCompare <- c("CREEHA_CONTROL", "CREEHA_10MMAA")
 
-file_goMap <- "E:/Chris_UM/Database/A_fumigatus_293_version_s03-m05-r06/A_fumigatus_Af293_orgDb/geneid2go.AFumigatus_Af293.topGO.map"
+file_goMap <- "E:/Chris_UM/Database/A_fumigatus_293_version_s03-m05-r06/annotation_resources/geneid2go.AFumigatus_Af293.topGO.map"
 file_exptInfo <- here::here("data", "referenceData/sampleInfo.txt")
 
 TF_dataPath <- here::here("data", "TF_data")
 
 sampleList <- c("CREEHA_CONTROL4", "CREEHA_CONTROL5", "CREEHA_10MMAA4", "CREEHA_10MMAA5")
-file_diffbindTargets <- here::here("analysis", "ChIPseq_analysis",
-                                   "peak_targets", "diffbind_allPeak_targets.tab")
+file_diffbindTargets <- here::here("analysis", "02_ChIPseq_analysis",
+                                   "01_peak_targets", "diffbind_allPeak_targets.tab")
 orgDb <- org.AFumigatus293.eg.db
 
 ##################################################################################
@@ -41,7 +42,7 @@ grp2Specific = paste(grp2, ":specific", sep = "")
 exptData <- get_sample_information(exptInfoFile = file_exptInfo,
                                    samples = sampleList,
                                    dataPath = TF_dataPath,
-                                   matrixSource = "normalizedmatrix")
+                                   profileMatrixSuffix = "normalizedmatrix")
 
 exptDataList <- purrr::transpose(exptData) %>%
   purrr::set_names(nm = purrr::map(., "sampleId"))
@@ -60,104 +61,206 @@ diffbindTargets <- dplyr::filter(diffbindTargets, pvalFilteredN > 0)
 dplyr::group_by(diffbindTargets, categoryDiffbind) %>% 
   dplyr::summarise(n = n())
 
+nodeSize <- 5
+
 ##################################################################################
 
-## genes specific to CREEHA_CONTROL sample
-nodeSize <- 5
-outPrefixTf1 <- paste(outPrefix, grp1, "_enriched", sep = "")
+## peaks with stronger binding in CREEHA_CONTROL sample
+outTf1Enrich <- paste(outPrefix, ".", grp1, "_enriched", sep = "")
 
-tf1StrongTargets <- dplyr::filter(.data = diffbindTargets,
+tf1Enrich <- dplyr::filter(.data = diffbindTargets,
                                   categoryDiffbind %in% c(grp1Enrich, grp1Specific))
 
-nrow(tf1StrongTargets)
+nrow(tf1Enrich)
 
-tf1Go <- topGO_enrichment(genes = unique(tf1StrongTargets$geneId),
+tf1EnrichGo <- topGO_enrichment(genes = unique(tf1Enrich$geneId),
                           goMapFile = file_goMap, goNodeSize = nodeSize)
 
-tf1Go$condition <- grp1Enrich
+tf1EnrichGo$condition <- grp1Enrich
 
-readr::write_tsv(x = tf1Go, path = paste(outPrefixTf1, ".tab", sep = ""))
+readr::write_tsv(x = tf1EnrichGo, path = paste(outTf1Enrich, ".tab", sep = ""))
 
-plotTitle <- paste(grp1, ": increased binding signal ( n =", length(unique(tf1StrongTargets$geneId)), ")")
+plotTitle <- paste(grp1, ": increased binding signal (n = ", length(unique(tf1Enrich$geneId)), ")",
+                   sep = "")
 
-tf1GoScatter <- topGO_scatterPlot(df = tf1Go, title = plotTitle)
+pt_tf1EnrichGo <- enrichment_scatter(df = tf1EnrichGo, title = plotTitle)
 
 ##################################################################################
-## genes specific to CREEHA_10MMAA3 sample
+## peaks specific to CREEHA_CONTROL sample
+outTf1Specific <- paste(outPrefix, ".", grp1, "_specific", sep = "")
 
-outPrefixTf2 <- paste(outPrefix, grp2, "_enriched", sep = "")
+tf1Specific <- dplyr::filter(.data = diffbindTargets,
+                             categoryDiffbind %in% c(grp1Specific))
 
-tf2StrongTargets <- dplyr::filter(.data = diffbindTargets,
+nrow(tf1Specific)
+
+tf1SpecificGo <- topGO_enrichment(genes = unique(tf1Specific$geneId),
+                          goMapFile = file_goMap, goNodeSize = nodeSize)
+
+tf1SpecificGo$condition <- grp1Specific
+
+readr::write_tsv(x = tf1SpecificGo, path = paste(outTf1Specific, ".tab", sep = ""))
+
+plotTitle <- paste(grp1, ": specific peaks (n = ", length(unique(tf1Specific$geneId)), ")",
+                   sep = "")
+
+pt_tf1SpecificGo <- enrichment_scatter(df = tf1SpecificGo, title = plotTitle)
+
+##################################################################################
+## peaks with stronger binding in CREEHA_10MMAA3 sample
+outTf2Enrich <- paste(outPrefix, ".", grp2, "_enriched", sep = "")
+
+tf2Enrich <- dplyr::filter(.data = diffbindTargets,
                                   categoryDiffbind %in% c(grp2Enrich, grp2Specific))
-nrow(tf2StrongTargets)
+nrow(tf2Enrich)
 
-tf2Go <- topGO_enrichment(genes = unique(tf2StrongTargets$geneId),
+tf2EnrichGo <- topGO_enrichment(genes = unique(tf2Enrich$geneId),
                           goMapFile = file_goMap, goNodeSize = nodeSize)
 
-tf2Go$condition <- grp2Enrich
+tf2EnrichGo$condition <- grp2Enrich
 
-readr::write_tsv(x = tf2Go, path = paste(outPrefixTf2, ".tab", sep = ""))
+readr::write_tsv(x = tf2EnrichGo, path = paste(outTf2Enrich, ".tab", sep = ""))
 
-plotTitle <- paste(grp2, ": increased binding signal ( n =", length(unique(tf2StrongTargets$geneId)), ")")
-tf2GoScatter <- topGO_scatterPlot(df = tf2Go, title = plotTitle)
+plotTitle <- paste(grp2, ": increased binding signal (n = ", length(unique(tf2Enrich$geneId)), ")",
+                   sep = "")
+
+pt_tf2EnrichGo <- enrichment_scatter(df = tf2EnrichGo, title = plotTitle)
 
 ##################################################################################
-## common targets for CREEHA_CONTROL2 and CREEHA_10MMAA3 samples
+## peaks with stronger binding in CREEHA_10MMAA3 sample
 
-outPrefixCommon <- paste(outPrefix, "common_noDiff", sep = "")
+outTf2Specific <- paste(outPrefix, ".", grp2, "_specific", sep = "")
 
-nodiffTargets <- dplyr::filter(.data = diffbindTargets, categoryDiffbind == "common")
+tf2Specific <- dplyr::filter(.data = diffbindTargets,
+                             categoryDiffbind %in% c(grp2Specific))
+nrow(tf2Specific)
 
-nrow(nodiffTargets)
+tf2SpecificGo <- topGO_enrichment(genes = unique(tf2Specific$geneId),
+                          goMapFile = file_goMap, goNodeSize = nodeSize)
 
-commonGo <- topGO_enrichment(genes = unique(nodiffTargets$geneId),
+tf2SpecificGo$condition <- grp2Specific
+
+readr::write_tsv(x = tf2SpecificGo, path = paste(outTf2Specific, ".tab", sep = ""))
+
+plotTitle <- paste(grp2, ": specific peaks (n = ", length(unique(tf2Specific$geneId)), ")",
+                   sep = "")
+
+pt_tf2SpecificGo <- enrichment_scatter(df = tf2SpecificGo, title = plotTitle)
+
+##################################################################################
+## common targets for CREEHA_CONTROL2 and CREEHA_10MMAA3 samples with no DiffBind change
+
+outcommonNoDiff <- paste(outPrefix, ".", "common_noDiff", sep = "")
+
+commonNoDiff <- dplyr::filter(.data = diffbindTargets, categoryDiffbind == "common")
+
+nrow(commonNoDiff)
+
+commonNoDiffGo <- topGO_enrichment(genes = unique(commonNoDiff$geneId),
                              goMapFile = file_goMap, goNodeSize = nodeSize)
 
-commonGo$condition <- "common"
+commonNoDiffGo$condition <- "common_noDiff"
 
-readr::write_tsv(x = commonGo, path = paste(outPrefixCommon, ".tab", sep = ""))
+readr::write_tsv(x = commonNoDiffGo, path = paste(outcommonNoDiff, ".tab", sep = ""))
 
-plotTitle <- paste("no change in binding signal between", grp1, "and",
-                   grp2, "( n =", length(unique(nodiffTargets$geneId)))
-commonGoScatter <- topGO_scatterPlot(df = commonGo, title = plotTitle)
+plotTitle <- paste("common peaks between ", grp1, " and ",
+                   grp2, "\nwith similar signal (n = ", length(unique(commonNoDiff$geneId)), ")",
+                   sep = "")
+pt_commonNoDiffGo <- enrichment_scatter(df = commonNoDiffGo, title = plotTitle)
+
+
+##################################################################################
+## common targets for CREEHA_CONTROL2 and CREEHA_10MMAA3 samples with no DiffBind change
+
+outCommonBound <- paste(outPrefix, ".", "common_bound", sep = "")
+
+commonBound <- dplyr::filter(.data = diffbindTargets,
+                             categoryDiffbind %in% c(grp1Enrich, "common", grp2Enrich))
+
+nrow(commonBound)
+
+commonBoundGo <- topGO_enrichment(genes = unique(commonBound$geneId),
+                                  goMapFile = file_goMap, goNodeSize = nodeSize)
+
+commonBoundGo$condition <- "common_bound"
+
+readr::write_tsv(x = commonBoundGo, path = paste(outCommonBound, ".tab", sep = ""))
+
+plotTitle <- paste("common peaks:", grp1, " and ",
+                   grp2, " (n = ", length(unique(commonBound$geneId)), ")",
+                   sep = "")
+pt_commonBoundGo <- enrichment_scatter(df = commonBoundGo, title = plotTitle)
 
 ##################################################################################
 
 aligned_plots <- align_plots(
-  plotlist = list(tf1 = tf1GoScatter, tf2 = tf2GoScatter, common = commonGoScatter),
+  plotlist = list(
+    tf1Enrich = pt_tf1EnrichGo,
+    tf1Specific = pt_tf1SpecificGo,
+    tf2Enrich = pt_tf2EnrichGo,
+    tf2Specific = pt_tf2SpecificGo,
+    commonNoDiff = pt_commonNoDiffGo,
+    commonBound = pt_commonBoundGo
+  ),
   align = "v")
 
-pdf(file = paste(outPrefix, "scatterplot.pdf", sep = ""),
+pdf(file = paste(outPrefix, ".scatterplot.pdf", sep = ""),
     width = 15, height = 14, onefile = T, pointsize = 18)
-ggdraw(aligned_plots$tf1)
-ggdraw(aligned_plots$tf2)
-ggdraw(aligned_plots$common)
+ggdraw(aligned_plots$tf1Enrich)
+ggdraw(aligned_plots$tf1Specific)
+ggdraw(aligned_plots$tf2Enrich)
+ggdraw(aligned_plots$tf2Specific)
+ggdraw(aligned_plots$commonNoDiff)
+ggdraw(aligned_plots$commonBound)
 dev.off()
 
 
 # wd <- (min(max(nchar(as.character(goData$Term))), 80) * 30) * 1.5
 wd <- 3500
-
-ht <- max(nrow(tf1Go) * 80, 1500)
-png(filename = paste(outPrefixTf1, "_scatterplot.png", sep = ""),
-    width = wd, height = ht, res = max(min(wd, ht) / 12, 200))
-ggdraw(aligned_plots$tf1)
+ht <- 4000
+# res = max(min(wd, ht) / 12, 200)
+res <- 300
+# ht <- max(nrow(tf1EnrichGo) * 80, 1500)
+png(filename = paste(outTf1Enrich, ".scatter.png", sep = ""),
+    width = wd, height = ht, res = res)
+ggdraw(aligned_plots$tf1Enrich)
 dev.off()
 
 
-ht <- max(nrow(tf2Go) * 80, 1500)
-png(filename = paste(outPrefixTf2, "_scatterplot.png", sep = ""),
-    width = wd, height = ht, res = max(min(wd, ht) / 12, 200))
-ggdraw(aligned_plots$tf2)
+# ht <- max(nrow(tf1SpecificGo) * 80, 1500)
+png(filename = paste(outTf1Specific, ".scatter.png", sep = ""),
+    width = wd, height = ht, res = res)
+ggdraw(aligned_plots$tf1Specific)
 dev.off()
 
-ht <- max(nrow(commonGo) * 80, 1500)
-png(filename = paste(outPrefixCommon, "_scatterplot.png", sep = ""),
-    width = wd, height = ht, res = max(min(wd, ht) / 12, 200))
-ggdraw(aligned_plots$common)
+# ht <- max(nrow(tf2EnrichGo) * 80, 1500)
+png(filename = paste(outTf2Enrich, ".scatter.png", sep = ""),
+    width = wd, height = ht, res = res)
+ggdraw(aligned_plots$tf2Enrich)
 dev.off()
 
 
+# ht <- max(nrow(tf2SpecificGo) * 80, 1500)
+png(filename = paste(outTf2Specific, ".scatter.png", sep = ""),
+    width = wd, height = ht, res = res)
+ggdraw(aligned_plots$tf2Specific)
+dev.off()
+
+
+# ht <- max(nrow(commonNoDiffGo) * 80, 1500)
+png(filename = paste(outcommonNoDiff, ".scatter.png", sep = ""),
+    width = wd, height = ht, res = res)
+ggdraw(aligned_plots$commonNoDiff)
+dev.off()
+
+# ht <- max(nrow(commonBoundGo) * 80, 1500)
+png(filename = paste(outCommonBound, ".scatter.png", sep = ""),
+    width = wd, height = ht, res = res)
+ggdraw(aligned_plots$commonBound)
+dev.off()
+
+
+##################################################################################
 
 
 
